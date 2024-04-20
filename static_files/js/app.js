2b3b34edbec7c7af428d7d493b38b3b8299991aa
ethankobrien <eobrien@wm.edu>
2024-04-20 18:47:30 -0400

    Update app.js

--- static_files/js/app.js
+++ static_files/js/app.js
@@ -1,9 +1,8 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FirstPage from "./languages";
-import SecondPage from "./recording";
-import ThirdPage from "./results";
+import FirstPage from "./recording";
+import SecondPage from "./results";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<FirstPage />} />
         <Route path="/second" element={<SecondPage />} />
-        <Route path="/third" element={<ThirdPage />} />
       </Routes>
     </Router>
   );
