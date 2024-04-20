76b05608c45c9719791731ff3ee3d86adb112e73
Shayaan Emran <shayaan.emran@gmail.com>
2024-04-20 12:50:18 -0400

    scripts

--- scripts/llama.py
+++ scripts/llama.py
@@ -1,9 +1,17 @@
 from meta_ai_api import MetaAI
 
-ai = MetaAI()
 def llama_call(prompt):
+    ai = MetaAI()
     response = ai.prompt(message = prompt)
     return response.get("message")
 
+def get_prompt(question):
+    file = open("data/prompt.txt", "r")
+    content = file.read()
+    return content
 
+def get_response(question):
+    prompt = get_prompt(question)
+    response = llama_call(prompt)
+    return response
  
