5ba6352b5e71b58a3f34625145f34c7a75e3bc3f
ethankobrien <eobrien@wm.edu>
2024-04-20 19:02:46 -0400

    js and html

--- static_files/js/app.js
+++ static_files/js/app.js
@@ -1,18 +1,27 @@
 // src/App.js
 import React from "react";
+import TopImage from './TopImage';
+import './App.css'; // Import CSS file for styling
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FirstPage from "./recording";
 import SecondPage from "./results";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path="/second" element={<SecondPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+          <Route path="/second" element={<SecondPage />} />
+        </Routes>
+      </Router>
+      <header>
+        <img src="top-image.png" alt="Top Image" className="top-image" />
+      </header>
+      {/* Other components */}
+    </div>
   );
 }
+
 
 export default App;
---
+++ static_files/js/topimage.js
@@ -0,0 +1,10 @@
+// TopImage.js
+import React from 'react';
+
+const TopImage = () => {
+  return (
+    <img src="top-image.png" alt="Top Image" className="top-image" />
+  );
+}
+
+export default TopImage;
