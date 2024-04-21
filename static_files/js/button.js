98d3a5715443bc787058361ab186d322c1ab57d4
tmrbbr <2023tbabar@tjhsst.edu>
2024-04-21 02:26:12 -0400

    combined  button js

--- static_files/js/button.js
+++ static_files/js/button.js
@@ -1,3 +1,36 @@
+let mediaRecorder;
+let audioChunks = [];
+navigator.mediaDevices.getUserMedia({ audio: true })
+.then(stream => {
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = event => {
+    audioChunks.push(event.data);
+    };
+
+    const recordButton = document.getElementById('recordBtn');
+    recordButton.onclick = () => {
+    if (mediaRecorder.state === 'recording') {
+        mediaRecorder.stop();
+        window.location = 'results';
+    } else {
+        mediaRecorder.start();
+        // recordButton.textContent = 'Stop'; REPLACE WITH REGULAR MIC
+        audioChunks = [];
+    }
+    };
+
+    mediaRecorder.onstop = () => {
+    const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+    const formData = new FormData();
+    formData.append('audioData', audioBlob);
+    fetch('/upload', {
+        method: 'POST',
+        body: formData,
+    }).then(response => response.text()).then(data => console.log(data));
+    };
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const recordButton = document.getElementById('recordBtn');
     const microphoneImage = document.querySelector('.microphone');
